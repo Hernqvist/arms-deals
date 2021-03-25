90cbe4b51810ca46473c24b7ba8af08530d91812
Hernqvist <fredrikhernqvist96@gmail.com>
2021-03-25 22:23:36 +0100

    Tweak loader

--- data_loader.py
+++ data_loader.py
@@ -1,5 +1,6 @@
 import json
 import sys
+import functools
 
 class Colors:
     """ ANSI color codes """
@@ -40,52 +41,65 @@ class Colors:
             kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
             del kernel32
 
+class LabelType:
+  def __init__(self, tag, color, priority):
+    self.tag = tag
+    self.color = color
+    self.priority = priority
+
+label_types = {}
+def add_layer_type(tag, color):
+  label_types[tag] = LabelType(tag, color, len(label_types))
+
+add_layer_type("Buyer Country", Colors.LIGHT_GREEN)
+add_layer_type("Buyer", Colors.YELLOW)
+add_layer_type("Seller Country", Colors.LIGHT_BLUE)
+add_layer_type("Seller", Colors.BLUE)
+add_layer_type("Price", Colors.CYAN)
+add_layer_type("Quantity", Colors.LIGHT_PURPLE)
+add_layer_type("Weapon", Colors.RED)
+add_layer_type("Date", Colors.DARK_GRAY)
+add_layer_type("Answer", Colors.END)
+
+ignore_labels = {"Buyer Country", "Seller Country"}
 
 # Variables that correspond directly to a json element are suffixed with _
 
 class Label:
-  colors = {
-    'Buyer':Colors.YELLOW,
-    'Seller':Colors.BLUE,
-    'Price':Colors.CYAN,
-    'Weapon':Colors.RED,
-    'Answer':Colors.END}
-
-  def __init__(self, content, start, end, type):
+  trimmed_chars = " ,.-"
+  def __init__(self, text, start, end, type):
+    while text[start] in self.trimmed_chars:
+      start += 1
+    while text[end-1] in self.trimmed_chars:
+      end -= 1
+    self.content = text[start:end]
+    self.type = type
     self.start = start
     self.end = end
-    self.content = content
-    self.type = type
 
   @classmethod
   def from_json(cls, text, label_):
     start, end = label_['start'], label_['end']
-    return cls(text[start:end], start, end, label_['marker'])
+    return cls(text, start, end, label_['marker'])
 
   @classmethod
-  def from_transpose(cls, label, offset):
-    return cls(label.content, label.start - offset, label.end - offset, label.type)
+  def from_transpose(cls, label, text, offset):
+    return cls(text[offset:], label.start - offset, label.end - offset, label.type)
 
 class Deal:
   def __init__(self, answer, labels):
     self.answer = answer
     self.start, self.end = self.answer.start, self.answer.end
-    self.buyers, self.sellers, self.prices, self.weapons = [], [], [], []
+    self.labels = {tag:[] for tag in label_types}
 
     for label in labels:
-      if label.start < self.start or label.end > self.end or label.type == 'Answer':
+      if label.start < self.start or label.end > self.end \
+          or label.type == 'Answer' or label.type in ignore_labels:
         continue
 
-      if label.type == "Buyer":
-        self.buyers.append(label)
-      elif label.type == "Seller":
-        self.sellers.append(label)
-      elif label.type == "Price":
-        self.prices.append(label)
-      elif label.type == "Weapon":
-        self.weapons.append(label)
+      self.labels[label.type].append(label) 
 
-    self.all_labels = self.buyers + self.sellers + self.prices + self.weapons
+    self.all_labels = functools.reduce(lambda x,y: x+y, self.labels.values())
 
   @classmethod
   def from_json(cls, text, answer_, labels_):
@@ -94,9 +108,9 @@ class Deal:
     return cls(answer, labels)
 
   @classmethod
-  def from_transpose(cls, deal, offset):
-    labels = [Label.from_transpose(label, offset) for label in deal.all_labels]
-    return cls(Label.from_transpose(deal.answer, offset), labels)
+  def from_transpose(cls, deal, text, offset):
+    labels = [Label.from_transpose(label, text, offset) for label in deal.all_labels]
+    return cls(Label.from_transpose(deal.answer, text, offset), labels)
 
 class Text:
   def __init__(self, text, deals):
@@ -132,25 +146,34 @@ class Text:
     for i in range(len(cutoffs) - 1):
       start, end = cutoffs[i], cutoffs[i+1]
       substring = self.text[start:end]
-      deal = Deal.from_transpose(self.deals[i], start)
+      deal = Deal.from_transpose(self.deals[i], self.text, start)
       texts.append(Text(substring, [deal]))
 
     return texts
       
 
   def colored_text(self):
-    ANSWER_COLOR = Label.colors['Answer'] + Colors.UNDERLINE
-    chars = [self.text[i] for i in range(len(self.text))]
+    ANSWER_COLOR = Colors.END + Colors.UNDERLINE
+    labels = [""]*len(self.text)
 
     for deal in self.deals:
+      for i in range(deal.start, deal.end):
+        labels[i] = "Answer"
       for label in deal.all_labels:
-        color = Label.colors[label.type]
-        chars[label.start] = color + chars[label.start] + Colors.UNDERLINE
-        chars[label.end - 1] = chars[label.end - 1] + ANSWER_COLOR
-      chars[deal.start] = ANSWER_COLOR + chars[deal.start]
-      chars[deal.end - 1] = chars[deal.end - 1] + Colors.END
+        for i in range(label.start, label.end):
+          if labels[i] == "" or label_types[labels[i]].priority > label_types[label.type].priority:
+            labels[i] = label.type
 
-    return "".join(chars)
+    text = ""
+    prev_label = ""
+    for label, char in zip(labels, self.text):
+      modifier = Colors.END + label_types[label].color + Colors.UNDERLINE if label != "" else ""
+      if label == "" and prev_label != "":
+        modifier = Colors.END
+      text = text + modifier + char
+      prev_label = label
+    text += Colors.END
+    return text
 
 class DataSet:
   def __init__(self, texts, name):
@@ -170,7 +193,6 @@ class DataSet:
   def split_deals(self):
     texts = []
     for text in self.texts:
-      print(texts, "{", text.split_deals(), "}")
       texts.extend(text.split_deals())
     return DataSet(texts, "Split of {}".format(self.name))
 
